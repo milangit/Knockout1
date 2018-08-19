ec5a9cec16d4e5cbcf27649493cbacaa12018678
Milan Bartoň <milangit@users.noreply.github.com>
2018-08-19 13:07:27 +0200

    Update viewmodel1.js

--- viewmodel1.js
+++ viewmodel1.js
@@ -1,7 +1,10 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 
+
+
 $(document).ready(function() {
-  function AppViewModel() {
+  var viewModel = {
+   
     this.firstName = ko.observable("Bert");
     this.lastName = ko.observable("Bertington");
     this.fullName = ko.computed(function() {
@@ -10,9 +13,9 @@ $(document).ready(function() {
     this.capitalizeLastName = function() {
         var currentVal = this.lastName();        // Read the current value
         this.lastName(currentVal.toUpperCase()); // Write back a modified value
-    };
-}
+    };    
+  }
   
   // Activates knockout.js
-  ko.applyBindings(new AppViewModel());
+  ko.applyBindings(viewModel);
 });
